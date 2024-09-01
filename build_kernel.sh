03fe336812d156d62bf81f2f83c9f2e4e1bb7048
hamjin <jinham@qq.com>
2024-09-01 12:04:54 +0800

    Refactor
    Signed-off-by: hamjin <jinham@qq.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,10 +17,10 @@ KERNEL_VERSION="$3"
 BUILD_TYPE="$4"
 
 # Build Number
-export BUILD_NUMBER="$5"
+BUILD_NUMBER="$5"
 
 # Support KernelSU
-KSU="$6"
+# KSU="$6"
 
 # Use Full LTO
 FULL_LTO="$7"
@@ -34,16 +34,41 @@ grep_prop() {
     return 0
 }
 
+grep_prop_space() {
+    local REGEX="s/^$1 = //p"
+    shift
+    local FILES=$@
+    cat $FILES 2>/dev/null | dos2unix | sed -n "$REGEX" | head -n 1
+
+    return 0
+}
+
 abort() {
     echo "$1"
 
     exit 1
 }
 
+get_number() {
+    echo "$1" | awk -F "" '
+{
+  for(i=1;i<=NF;i++) 
+  {  
+    if ($i ~ /[[:digit:]]/)     
+    {
+      str=$i
+      str1=(str1 str)
+    }  
+  } 
+  print str1
+}'
+}
+
 # Exports
 export PATH="$CLANG_PATH:$PATH"
-export CC="ccache $CLANG_PATH/clang"
-export CXX="ccache $CLANG_PATH/clang++"
+export CCACHE="ccache"
+export CC="$CLANG_PATH/clang"
+export CXX="$CLANG_PATH/clang++"
 export CLANG_TRIPLE="aarch64-linux-gnu"
 export CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
 export CROSS_COMPILE="aarch64-linux-gnu-"
@@ -52,58 +77,75 @@ export SUBARCH="$ARCH"
 export KBUILD_BUILD_USER="GHCI"
 export KBUILD_BUILD_HOST="Pandora"
 export DEFCONFIG="gki_defconfig"
-export BRANCH
-export KMI_GENERATION
-export LOCALVERSION
+MODULE_VER=""
+MODULE_VERCODE=""
 
 # Resources
-THREADS="$(nproc --all)"
-
-# Paths
-KERNEL_DIR="$(pwd)"
-INSTALL_MOD_PATH="$KERNEL_DIR/magisk/kernel_modules"
-OUT_DIR="$KERNEL_DIR/.out"
-MOD_DIR="$KERNEL_DIR/out"
-
-# Kernel Branch and KMI Generation
-BRANCH="$(grep_prop "BRANCH" "$KERNEL_DIR/build.config.constants")"
-[ -z "$BRANCH" ] && BRANCH="$(grep_prop "BRANCH" "$KERNEL_DIR/build.config.common")"
-KMI_GENERATION="$(grep_prop "KMI_GENERATION" "$KERNEL_DIR/build.config.common")"
-android_release=$(echo "$BRANCH" | sed -e '/android[0-9]\{2,\}/!{q255};s/^\(android[0-9]\{2,\}\)-.*/\1/')
-kernel_version="$(echo "$BRANCH" | awk -F- '{print $2}')"
-
+THREADS="$(($(nproc --all) - 1))"
 TIME="$(date +"%H%M%S")"
 WEEK="$(date +"%gw%V")"
 DAY="$(printf "\x$(printf %x $((96 + $(date +"%u"))))")"
 
-# Vars
-BETA_VERSION=""
+# Paths
+KERNEL_DIR="$(pwd)"
+INSTALL_MOD_PATH="$KERNEL_DIR/magisk/kernel_modules"
+OUT_DIR="$KERNEL_DIR/../build_dir"
+MOD_DIR="$KERNEL_DIR/../out_dir"
 
-case "$BUILD_TYPE" in
-"REL")
-    BUILD_NUMBER="n"
-    BETA_VERSION="$KERNEL_VERSION"
-    ;;
-"BETA")
-    BUILD_NUMBER="n"
-    BETA_VERSION="$WEEK$DAY"
-    ;;
-"DEBUG")
-    BETA_VERSION="$WEEK$DAY"
-    ;;
-*)
-    BUILD_NUMBER="n"
-    BETA_VERSION="$DEV"
-    ;;
-esac
-LOCALVERSION="-$KERNEL_NAME-$BETA_VERSION"
+# Kernel Branch and KMI Generation
+get_kernel_version() {
+    local BRANCH
+    local KMI_GENERATION
+    local time
 
-[ -z "$BUILD_NUMBER" ] && BUILD_NUMBER="$(date +"%s")"
-[ "$BUILD_NUMBER" = "n" ] && BUILD_NUMBER=""
-BUILD_NUMBER="$(echo "$BUILD_NUMBER" | cut -c1-8)"
-SCM_VERSION="$android_release-$KMI_GENERATION-g$(git rev-parse --verify HEAD | cut -c1-12)"
-FULL_VERSION="$KERNEL_NAME-$BETA_VERSION-$SCM_VERSION"
-[ -z "$BUILD_NUMBER" ] && echo "$TIME: $FULL_VERSION" || echo "$TIME: $FULL_VERSION-ab$BUILD_NUMBER"
+    time="$(date +"%H:%M:%S")"
+
+    VERSION="$(grep_prop_space VERSION "$KERNEL_DIR/Makefile")"
+    PATCHLEVEL="$(grep_prop_space PATCHLEVEL "$KERNEL_DIR/Makefile")"
+    SUBLEVEL="$(grep_prop_space SUBLEVEL "$KERNEL_DIR/Makefile")"
+
+    KERNEL_VER="$VERSION.$PATCHLEVEL.$SUBLEVEL"
+    KERNEL_LOCALVER=$(grep_prop "CONFIG_LOCALVERSION" "$OUT_DIR/.config" | tr -d '"')
+
+    BRANCH="$(grep_prop "BRANCH" "$KERNEL_DIR/build.config.constants")"
+    [ -z "$BRANCH" ] && BRANCH="$(grep_prop "BRANCH" "$KERNEL_DIR/build.config.common")"
+    KMI_GENERATION="$(grep_prop "KMI_GENERATION" "$KERNEL_DIR/build.config.common")"
+    android_release=$(echo "$BRANCH" | sed -e '/android[0-9]\{2,\}/!{q255};s/^\(android[0-9]\{2,\}\)-.*/\1/')
+    kernel_version="$(echo "$BRANCH" | awk -F- '{print $2}')"
+
+    BETA_VERSION=""
+    case "$BUILD_TYPE" in
+    "REL")
+        BETA_VERSION="$KERNEL_VERSION"
+        BUILD_NUMBER="n"
+        MODULE_VER="$BETA_VERSION"
+        MODULE_VERCODE="$(get_number "$MODULE_VER")"
+        ;;
+    "BETA")
+        BETA_VERSION="$WEEK$DAY"
+        MODULE_VER="$BETA_VERSION"
+        MODULE_VERCODE="$(date +"%g%m%d")"
+        ;;
+    *)
+        BETA_VERSION="DEV"
+        BUILD_NUMBER="n"
+        MODULE_VERCODE="$(date +"%s")"
+        MODULE_VER="$BETA_VERSION\_$(date +"%g%m%d%H%M%S")"
+        ;;
+    esac
+
+    [ -z "$BUILD_NUMBER" ] && BUILD_NUMBER="$(date +"%s")"
+    [ "$BUILD_NUMBER" = "n" ] && BUILD_NUMBER=""
+    BUILD_NUMBER="$(echo "$BUILD_NUMBER" | cut -c1-8)"
+    KMI_VER="$android_release-$KMI_GENERATION"
+    SCM_VERSION="$KMI_VER-g$(git rev-parse --verify HEAD | cut -c1-12)"
+    FULL_VERSION="$KERNEL_VER-$KERNEL_NAME-$BETA_VERSION-$SCM_VERSION"
+    [ -n "$BUILD_NUMBER" ] && FULL_VERSION="$FULL_VERSION-ab$BUILD_NUMBER"
+    FULL_VERSION="$FULL_VERSION$KERNEL_LOCALVER"
+    export KERNELRELEASE="$FULL_VERSION"
+    echo "$time: $FULL_VERSION"
+}
+get_kernel_version
 
 ZIMAGE_DIR="$OUT_DIR/arch/arm64/boot"
 DATE_BEGIN=$(date +"%s")
@@ -114,29 +156,24 @@ echo "Making Kernel:"
 echo "-------------------"
 echo
 rm -rf "$MOD_DIR"
-make CC="$CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" ARCH=$ARCH $DEFCONFIG -j"$THREADS"
-make CC="$CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" savedefconfig -j"$THREADS"
-if $KSU; then
-    echo "-------------------"
-    echo "Enabling KSU"
-    echo "-------------------"
-    "$KERNEL_DIR/scripts/config" --file "$OUT_DIR/.config" -e KSU
-fi
+make CC="$CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" ARCH=$ARCH KERNELRELEASE="$FULL_VERSION" $DEFCONFIG -j"$THREADS"
 if $FULL_LTO; then
     echo "-------------------"
     echo "Enabling FullLTO"
     echo "-------------------"
+    "$KERNEL_DIR/scripts/config" --file "$OUT_DIR/.config" -d LTO_NONE
     "$KERNEL_DIR/scripts/config" --file "$OUT_DIR/.config" -d LTO_CLANG_THIN
     "$KERNEL_DIR/scripts/config" --file "$OUT_DIR/.config" -e LTO_CLANG_FULL
 else
     echo "-------------------"
     echo "Enabling ThinLTO"
     echo "-------------------"
-    "$KERNEL_DIR/scripts/config" --file "$OUT_DIR/.config" -d LTO_CLANG_Full
+    "$KERNEL_DIR/scripts/config" --file "$OUT_DIR/.config" -d LTO_NONE
+    "$KERNEL_DIR/scripts/config" --file "$OUT_DIR/.config" -d LTO_CLANG_FULL
     "$KERNEL_DIR/scripts/config" --file "$OUT_DIR/.config" -e LTO_CLANG_Thin
 fi
-make CC="$CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" ARCH=$ARCH -j"$THREADS" Image modules Image.gz 2>&1
-make CC="$CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" modules_install INSTALL_MOD_PATH="$MOD_DIR" INSTALL_MOD_STRIP=1 ARCH=$ARCH -j"$THREADS"
+make CC="$CCACHE $CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" ARCH=$ARCH KERNELRELEASE="$FULL_VERSION" -j"$THREADS" Image modules 2>&1
+make CC="$CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" ARCH=$ARCH KERNELRELEASE="$FULL_VERSION" INSTALL_MOD_PATH="$MOD_DIR" INSTALL_MOD_STRIP=1 -j"$THREADS" modules_install 2>&1
 
 DATE_END="$(date +"%s")"
 DIFF="$((DATE_END - DATE_BEGIN))"
@@ -150,24 +187,31 @@ echo "-------------------"
 echo "Installing modules"
 echo "-------------------"
 mkdir -p "$INSTALL_MOD_PATH"
-cp -af "$MOD_DIR"/lib/modules/*/kernel/drivers/block/zram/zram.ko "$INSTALL_MOD_PATH"/zram.ko
-cd "$KERNEL_DIR"/magisk || abort "No magisk module!"
-7zz a -mx1 -mmt"$THREADS" '-xr!config/old' '-xr!config/origin' magisk.zip ./* >/dev/null 2>&1
+for MOD in zram perfmgr; do
+    find "$MOD_DIR" -name "$MOD".ko -exec cp -af {} "$INSTALL_MOD_PATH" \;
+done
 
 echo "-------------------"
 echo "Making magisk module"
 echo "-------------------"
-mkdir -p "$OUT_DIR"/tmp
-cp -fp "$ZIMAGE_DIR"/Image.gz "$OUT_DIR"/tmp
-cp -afrp "$KERNEL_DIR"/anykernel/* "$OUT_DIR"/tmp
-mv -f "$KERNEL_DIR/magisk/magisk.zip" "$OUT_DIR"/tmp/
-
-cd "$OUT_DIR"/tmp || abort "No anykernel!"
+cp -af "$KERNEL_DIR"/magisk "$OUT_DIR/"
+cd "$OUT_DIR"/magisk || abort "No magisk module!"
+sed -i "s#STATIC_VERSION#$MODULE_VER#g" module.prop
+sed -i "s#STATIC_VERCODE#$MODULE_VERCODE#g" module.prop
+7zz a -mx1 -mmt"$THREADS" '-xr!config/old' '-xr!config/origin' magisk.zip ./* >/dev/null 2>&1
 
 echo "-------------------"
 echo "Making flash package"
 echo "-------------------"
-7zz a -mx1 -mmt"$THREADS" tmp.zip ./* >/dev/null 2>&1
+cp -af "$KERNEL_DIR"/anykernel "$OUT_DIR"
+cp -af "$ZIMAGE_DIR"/Image "$OUT_DIR"/anykernel/
+mv -f magisk.zip "$OUT_DIR"/anykernel/
+cd "$OUT_DIR"/anykernel/ || abort "No anykernel!"
+7zz a -mx1 -mmt"$THREADS" anykernel.zip ./* >/dev/null 2>&1
+
+echo "-------------------"
+echo "Outputting flash package"
+echo "-------------------"
 cd .. || abort "Dir missing!"
 rm -f "$KERNEL_DIR"/*.zip
 ZIPNAME="Kernel-$KERNEL_NAME-$kernel_version"
@@ -175,9 +219,11 @@ case "$BUILD_TYPE" in
 "REL") ZIPNAME="$ZIPNAME-$BETA_VERSION" ;;
 *) ZIPNAME="$ZIPNAME-$BETA_VERSION-$TIME" ;;
 esac
-mv -f "$OUT_DIR"/tmp/tmp.zip "$KERNEL_DIR/$ZIPNAME".zip
-rm -rf "$OUT_DIR"/tmp
+
+mv -f "$OUT_DIR"/anykernel/anykernel.zip "$KERNEL_DIR/$ZIPNAME".zip
+
 echo "-------------------"
 echo "Output: $ZIPNAME.zip"
 echo "-------------------"
+
 echo "$ZIPNAME" >"$KERNEL_DIR/.output"
---
+++ build_kernel_old.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# $1:CLANG_PATH $2:VERSION $3:DEVICE
+# Compiler
+CLANG_PATH="$1"
+
+# Kernel Version
+KERNEL_VERSION="$2"
+
+# Build Type
+DEVICE="$3"
+
+abort() {
+    echo "$1"
+
+    exit 1
+}
+
+# Exports
+KERNEL_NAME="BocchiTheKernel"
+export PATH="$CLANG_PATH:$PATH"
+export CCACHE="ccache"
+export CC="$CLANG_PATH/clang"
+export CXX="$CLANG_PATH/clang++"
+export CLANG_TRIPLE="aarch64-linux-gnu"
+export CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
+export CROSS_COMPILE="aarch64-linux-gnu-"
+export ARCH="arm64"
+export SUBARCH="$ARCH"
+export KBUILD_BUILD_USER="Laulan56"
+export KBUILD_BUILD_HOST="GHCI"
+export DEFCONFIG="${DEVICE}_defconfig"
+export BRANCH
+export KMI_GENERATION
+export LOCALVERSION
+
+# Resources
+THREADS="$(($(nproc --all) - 1))"
+
+# Paths
+KERNEL_DIR="$(pwd)"
+OUT_DIR="$KERNEL_DIR/../build_dir"
+
+LOCALVERSION="-$KERNEL_VERSION"
+
+TIME="$(date +"%H%M%S")"
+FULL_VERSION="BocchiTheKernel-$KERNEL_VERSION"
+echo "$TIME: $FULL_VERSION"
+
+ZIMAGE_DIR="$OUT_DIR/arch/arm64/boot"
+DATE_BEGIN=$(date +"%s")
+
+echo
+echo "-------------------"
+echo "Making Kernel:"
+echo "-------------------"
+echo
+make CC="$CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" ARCH=$ARCH "$DEFCONFIG" -j"$THREADS"
+make CC="$CCACHE $CC" LLVM=1 LLVM_IAS=1 O="$OUT_DIR" ARCH=$ARCH -j"$THREADS" Image Image.gz
+
+DATE_END="$(date +"%s")"
+DIFF="$((DATE_END - DATE_BEGIN))"
+echo "-------------------"
+echo "Build Completed, Time: $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds."
+echo "-------------------"
+
+ls -a "$ZIMAGE_DIR"
+
+echo "-------------------"
+echo "Making magisk module"
+echo "-------------------"
+cd "$KERNEL_DIR"/magisk || abort "No magisk module!"
+7zz a -mx1 -mmt"$THREADS" magisk.zip ./* >/dev/null
+
+mkdir -p "$OUT_DIR"/tmp
+cp -fp "$ZIMAGE_DIR"/Image.gz "$OUT_DIR"/tmp
+cp -afrp "$KERNEL_DIR"/anykernel/* "$OUT_DIR"/tmp
+mv -f "$KERNEL_DIR/magisk/magisk.zip" "$OUT_DIR"/tmp/
+
+cd "$OUT_DIR"/tmp || abort "No anykernel!"
+
+echo "-------------------"
+echo "Making flash package"
+echo "-------------------"
+7zz a -mx1 -mmt"$THREADS" tmp.zip ./* >/dev/null 2>&1
+cd .. || abort "Dir missing!"
+rm -f "$KERNEL_DIR"/*.zip
+case "$DEVICE" in
+"odin") DEVICE_NAME="MIX4" ;;
+"haydn") DEVICE_NAME="K40P" ;;
+"venus") DEVICE_NAME="MI11" ;;
+"star") DEVICE_NAME="MI11PU" ;;
+*) DEVICE_NAME="ERROR" ;;
+esac
+ZIPNAME="$KERNEL_NAME-$KERNEL_VERSION-$DEVICE_NAME"
+
+mv -f "$OUT_DIR"/tmp/tmp.zip "$KERNEL_DIR/$ZIPNAME".zip
+rm -rf "$OUT_DIR"/tmp
+echo "-------------------"
+echo "Output: $ZIPNAME.zip"
+echo "-------------------"
+echo "$ZIPNAME" >"$KERNEL_DIR/.output"
